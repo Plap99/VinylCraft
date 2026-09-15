375051f0858fb216e9ccc308d15eacac03199913
Radig <incplap@gmail.com>
2026-09-15 02:20:26 -0600

    Mejora el modelo y orientación del Reproductor de Vinilos

--- src/main/java/com/radig/vinylcraft/block/VinylPlayerBlock.java
+++ src/main/java/com/radig/vinylcraft/block/VinylPlayerBlock.java
@@ -1,19 +1,54 @@
 package com.radig.vinylcraft.block;
 
+import com.mojang.serialization.MapCodec;
+
 import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
+import net.minecraft.world.item.context.BlockPlaceContext;
 import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.HorizontalDirectionalBlock;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.StateDefinition;
 import net.minecraft.world.phys.shapes.CollisionContext;
 import net.minecraft.world.phys.shapes.VoxelShape;
 
-public class VinylPlayerBlock extends Block {
+public class VinylPlayerBlock extends HorizontalDirectionalBlock {
 
     private static final VoxelShape SHAPE =
             Block.box(1, 0, 1, 15, 8, 15);
 
+    public static final MapCodec<VinylPlayerBlock> CODEC =
+            simpleCodec(VinylPlayerBlock::new);
+
     public VinylPlayerBlock(Properties properties) {
         super(properties);
+
+        registerDefaultState(
+                stateDefinition.any()
+                        .setValue(FACING, Direction.NORTH)
+        );
+    }
+
+    @Override
+    protected MapCodec<? extends HorizontalDirectionalBlock> codec() {
+        return CODEC;
+    }
+
+    @Override
+    public BlockState getStateForPlacement(BlockPlaceContext context) {
+        return defaultBlockState()
+                .setValue(
+                        FACING,
+                        context.getHorizontalDirection().getOpposite()
+                );
+    }
+
+    @Override
+    protected void createBlockStateDefinition(
+            StateDefinition.Builder<Block, BlockState> builder) {
+
+        builder.add(FACING);
     }
 
     @Override
